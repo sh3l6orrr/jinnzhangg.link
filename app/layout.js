61a45089b021e97349c191f30019f9185ab1b9de
Jin Zhang <sh3l6or@icloud.com>
2024-01-13 11:56:43 -0800

    add analytics

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,14 @@ export const metadata = {
   title: "Jin's vantage",
   description: "Jin's personal website",
 }
-
+import { Analytics } from '@vercel/analytics/react';
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-white dark:bg-black">
-      <body >{children}</body>
+      <body >
+        {children}
+      <Analytics />
+      </body>
     </html>
   )
 }
